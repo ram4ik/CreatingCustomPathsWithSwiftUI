7327ec18b76f2400d6382119970ebcb54ffda7dc
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-05 13:24:41 +0200

    init

--- CreatingCustomPathsWithSwiftUI/ContentView.swift
+++ CreatingCustomPathsWithSwiftUI/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+        }
+        .stroke(Color.blue.opacity(0.25), lineWidth: 10)
     }
 }
 
